2ed4d14d264ac0085cd96a596eabfb03f21cb7be
Sarahdeb1305 <debozzisarah@hotmail.com>
2025-07-24 10:40:45 +0200

    Ajout de la page galerie

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,7 +1,10 @@
 import Route from "./Route.js";
 
 //Définir vos routes ici
-export const allRoutes = [new Route("/", "Accueil", "/pages/home.html")];
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html")
+];
 
 //le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Quai Antique";
